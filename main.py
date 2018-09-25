2557352f967930724e8916dcfb0705440ac839d5
TarCode <tarcode33@gmail.com>
2018-09-25 14:03:40 +0200

    added .gitignore and query poloniex for last pair price

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import os
 import time
 import sys, getopt
 import datetime
+from poloniex import Poloniex
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def main(argv):
     period = 10
+    pair="BTC_ETH"
 
     try:
-        opts, args = getopt.getopt(argv, "hp:", ["period=",])
+        opts, args = getopt.getopt(argv, "hp:c:", ["period=","currency="])
     except getopt.GetoptError:
-        print('main.py -p <period>')
+        print('main.py -p <period> -c <currency pair>')
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print("main.py -p <period>")
+            print("main.py -p <period> -c <currency pair>")
             sys.exit()
         elif opt in ("-p", "--period"):
             if int(arg) in [300, 900, 1800, 7200, 14400, 86400]:
@@ -21,8 +28,15 @@ def main(argv):
             else:
                 print("Poloniex requires periods in 300, 900, 1800, 7200, 14400, 86400")
                 sys.exit(2)
+
+    KEY = os.getenv("POLONIEX_KEY")
+    SECRET = os.getenv("POLONIEX_SECRET")
+    conn = Poloniex(KEY, SECRET)
+
     while True:
-        print("{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()) + " Period ", period )
+        currentValues = conn.returnTicker()
+        lastPairPrice = currentValues[pair]["last"]
+        print("{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()) + " Period ", period, pair, lastPairPrice )
         time.sleep(int(period))
 
 if __name__ == "__main__":
