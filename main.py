d8ed5b4f010eed17b52aca6c0f7195bfabd1fd3f
TarCode <tarcode33@gmail.com>
2018-09-25 14:25:25 +0200

    calculate current moving avg

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ load_dotenv()
 def main(argv):
     period = 10
     pair="BTC_ETH"
+    prices = []
+    lengthOfMA = 0
+    currentMovingAverage = 0
 
     try:
-        opts, args = getopt.getopt(argv, "hp:c:", ["period=","currency="])
+        opts, args = getopt.getopt(argv, "hp:c:n:", ["period=","currency=", "points="])
     except getopt.GetoptError:
-        print('main.py -p <period> -c <currency pair>')
+        print('main.py -p <period> -c <currency pair> -n <period of moving average>')
         sys.exit(2)
 
     for opt, arg in opts:
@@ -23,12 +26,15 @@ def main(argv):
             print("main.py -p <period> -c <currency pair>")
             sys.exit()
         elif opt in ("-p", "--period"):
-            if int(arg) in [300, 900, 1800, 7200, 14400, 86400]:
+            if int(arg) in [10, 300, 900, 1800, 7200, 14400, 86400]:
                 period = arg
             else:
                 print("Poloniex requires periods in 300, 900, 1800, 7200, 14400, 86400")
                 sys.exit(2)
-
+        elif opt in ("-c", "--currency"):
+            pair = arg
+        elif opt in ("-n", "--points"):
+            lengthOfMA = int(arg)
     KEY = os.getenv("POLONIEX_KEY")
     SECRET = os.getenv("POLONIEX_SECRET")
     conn = Poloniex(KEY, SECRET)
@@ -36,7 +42,14 @@ def main(argv):
     while True:
         currentValues = conn.returnTicker()
         lastPairPrice = currentValues[pair]["last"]
-        print("{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()) + " Period ", period, pair, lastPairPrice )
+
+        if len(prices) > 0:
+            currentMovingAverage = sum(prices) / float(len(prices))
+
+
+        print("{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()) + " Period " + period + "s" + " Pair: " + pair + " Last pair price: " + str(lastPairPrice) + " Curr moving avg: " + str(currentMovingAverage) )
+        prices.append(float(lastPairPrice))
+        # prices = prices[-lengthOfMA]
         time.sleep(int(period))
 
 if __name__ == "__main__":
